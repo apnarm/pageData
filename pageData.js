624707390b3d34b583b406676a6a49418f879815
Mark Gandolfo <mark@aussiev8.com.au>
2014-05-08 13:54:58 +1000

    removing functions from pagedata

--- pageData.js
+++ pageData.js
@@ -1,6 +1,6 @@
 javascript:(function(){
 
-  this.isFunction = function(functionToCheck) {
+  function isFunction(functionToCheck) {
     var getType = {};
     return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
   }
@@ -19,7 +19,7 @@ javascript:(function(){
   var html = "<h2>Page Data</h2><tr><th>Name</th><th>Value</th></tr>";
 
   for(var prop in pg) {
-    if(prop != "cleanDirtyArray") {
+    if(!isFunction(prop)) {
       html = html + "<tr><td>"+ prop +"</td><td>"+ pg[prop] +"</td></tr>";
     }
   }
